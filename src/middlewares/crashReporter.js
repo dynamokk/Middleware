4bba0638094836438234a2cb39ab4ce07e33406a
dynamokk <a.ovsiannikov@levi9.com>
2016-11-06 20:37:45 +0200

    Add files via upload

---
+++ src/middlewares/crashReporter.js
@@ -0,0 +1,16 @@
+import {Raven} from 'raven-js'
+
+export const crashReporter = store => next => action => {
+    try {
+      return next(action)
+    } catch (err) {
+      console.error('Caught an exception!', err)
+      Raven.captureException(err, {
+        extra: {
+          action,
+          state: store.getState()
+        }
+      })
+      throw err
+    }
+}
